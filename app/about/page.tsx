78201394f198cf9f63334e3e9ab95bb5b0ea832f
hamkke <walwalzz@naver.com>
2024-07-29 00:17:19 +0900

    about 페이지 구현

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,15 @@
-import Demo from '@/components/demo';
+import { Metadata } from 'next';
+import styles from '@/styles/about.module.css';
+
+export const metadata: Metadata = {
+  title: 'about',
+};
 
 const About = () => {
   return (
-    <div>
-      About
-      <Demo />
+    <div className={styles.container}>
+      <h2 className={styles.title}>（っ ‘ ᵕ ‘ ｃ）</h2>
+      <p className={styles.p}>HELLOOOOOOOO</p>
     </div>
   );
 };
