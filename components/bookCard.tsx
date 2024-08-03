005a24e52590c935db1eff1eecf7399aea867507
hamkke <walwalzz@naver.com>
2024-08-03 16:54:52 +0900

    설명 추가

--- components/bookCard.tsx
+++ components/bookCard.tsx
@@ -19,9 +19,28 @@ export default function BookCard({
   const [isBright, setIsBright] = useState(false);
   const exceptForMeRef = useRef<HTMLButtonElement>(null);
 
+  /**
+1.이미지 요청 url을 next.cofig.js에서 rewrites 설정한대로 변경한다(fixedImagPath)
+2. useColor hooks 사용
+3. 받아온 data를 isColorBright이용해서 밝기 체크
+4. retrun값으로 클래스 적용
+ */
+  useEffect(() => {
+    const regex = /\/[^/]+\.jpg$/;
+    const match = imgPath.match(regex);
+    if (match) {
+      setFixedImgPath(`/transform/${match[0]}`);
+    }
+  }, [imgPath, fixedImgPath]);
+
   const { data, loading } = useColor(fixedImgPath, 'hex', {
-    crossOrigin: 'Anonymous',
-    quality: 1,
+    // useColor(이미지url, 형식, 옵션{crossOrigin, quality})
+    crossOrigin: 'Anonymous', // 이거 아무 쓸모 없어요
+    quality: 40,
+    /**
+    quality: 1이면, 모든 픽셀을 샘플링, 속도 저하
+    quality: 10이면, 10픽셀마다 하나의 픽셀을 샘플링, 속도 증가
+     */
   });
 
   const isColorBright = (hexColor: string | undefined) => {
@@ -33,35 +52,39 @@ export default function BookCard({
     return brightness >= 155;
   };
 
+  /**
+isColorBright('#000')
+brightness = 0 
+isColorBright('#fff')
+brightness = 255
+밝기 값의 범위는 0부터 255까지입니다
+0은 완전한 검정색을 의미하고,
+255는 완전한 흰색을 의미합니다.
+0부터255까지 중간값을 사용하지 않고 약간 높은 155를 사용하는지 gpt에게 물어보니 가독성을 보장하기 위해라고 합니다
+ */
+
   useEffect(() => {
     setIsBright(isColorBright(data));
   }, [data]);
 
   useEffect(() => {
-    const regex = /\/[^/]+\.jpg$/;
-    const match = imgPath.match(regex);
-    if (match) {
-      setFixedImgPath(`/transform/${match[0]}`);
-    }
-  }, [imgPath, fixedImgPath]);
-
-  useEffect(() => {
-    const closeLinkButton = (e: MouseEvent) => {
+    const closeBuyLinkButton = (e: MouseEvent) => {
       if (
         exceptForMeRef.current &&
-        !exceptForMeRef.current.contains(e.target as Node)
+        !exceptForMeRef.current.contains(e.target as HTMLElement)
       ) {
+        // console.log(e.target, e.currentTarget); // 둘이 다름
         setIsOpen(false);
       }
     };
 
-    document.addEventListener('mousedown', closeLinkButton);
+    document.addEventListener('mousedown', closeBuyLinkButton);
     return () => {
-      document.removeEventListener('mousedown', closeLinkButton);
+      document.removeEventListener('mousedown', closeBuyLinkButton);
     };
   }, []);
 
-  const toggleLinkButton = () => {
+  const toggleBuyLinkButton = () => {
     setIsOpen((prev) => !prev);
   };
 
@@ -70,7 +93,6 @@ export default function BookCard({
     <li className={styles.container} style={{ backgroundColor: data }}>
       <div className={styles.imgContainer}>
         <Image
-          className=''
           src={imgPath}
           alt={title}
           priority
@@ -80,7 +102,6 @@ export default function BookCard({
           sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
         />
       </div>
-      {/* // ---------------- */}
       <div
         className={`${
           isBright ? styles.fontColorBlack : styles.fontColorWhite
@@ -93,7 +114,7 @@ export default function BookCard({
             className={`${styles.buttonCommon} ${styles.linkButton} ${
               isBright ? styles.linkButtonBlack : styles.linkButtonWhite
             }`}
-            onClick={toggleLinkButton}
+            onClick={toggleBuyLinkButton}
             ref={exceptForMeRef}
           >
             BUY
@@ -107,7 +128,6 @@ export default function BookCard({
             </button> */}
         </div>
       </div>
-      {/* // ---------------- */}
       <span className={styles.rankCircle}>{rank}</span>
     </li>
   );
